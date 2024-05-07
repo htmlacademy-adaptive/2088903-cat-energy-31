a0fd69a7e9c49f969f618001692a1335bb27de47
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2024-05-08 02:15:48 +0300

    adds tablet styles

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
-/* в этот файл добавляет скрипты*/
+const menuBtn = document.getElementById('mobile-menu');
+
+const onMenuClick = () => {
+  if (menuBtn.classList.contains('header__mobile-menu--opened')) {
+    menuBtn.classList.remove('header__mobile-menu--opened');
+  } else {
+    menuBtn.classList.add('header__mobile-menu--opened');
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => menuBtn.addEventListener('click', onMenuClick));
