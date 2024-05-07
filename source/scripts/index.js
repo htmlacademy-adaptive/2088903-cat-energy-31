a36a5026cd91a63562b6b45d902e7d6c2f02c0b7
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2024-05-08 02:15:48 +0300

    adds mobile grids

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,13 @@
-/* в этот файл добавляет скрипты*/
+const menuBtn = document.querySelector('.header__mobile-menu');
+
+const clickHandler = () => {
+  if (menuBtn.classList.contains('header__mobile-menu--opened')) {
+    menuBtn.classList.remove('header__mobile-menu--opened');
+    menuBtn.classList.add('header__mobile-menu--closed');
+  } else {
+    menuBtn.classList.remove('header__mobile-menu--closed');
+    menuBtn.classList.add('header__mobile-menu--opened');
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => menuBtn.addEventListener('click', clickHandler));
