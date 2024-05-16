3cde310ef146113a64532ebda10fb14679d7fa7f
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2024-05-16 23:46:14 +0300

    Module6 task1 (#4)
    * adds tablet styles
    
    * adds example tablet stles
    
    * adds contacts style
    
    * fixes issues
    
    * adds mobile hero background style
    
    * adds tablet programs style

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,4 @@
+// Menu button
 const menuBtn = document.getElementById('mobile-menu');
 
 const onMenuClick = () => {
@@ -9,3 +10,15 @@ const onMenuClick = () => {
 };
 
 document.addEventListener('DOMContentLoaded', () => menuBtn.addEventListener('click', onMenuClick));
+
+
+// Slider
+/* const slider = document.getElementById('slider');
+const slideContainer = document.getElementById('img1');
+
+const onInputChange = () => {
+  slideContainer.style.width = `${slider.value}%`;
+  slider.style.setProperty('--pseudo-content', `${slider.value - 1}%`);
+};
+
+document.addEventListener('DOMContentLoaded', () => slider.addEventListener('change', onInputChange)); */
