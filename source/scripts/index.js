4fead094362e4ced32eb7d9ecf7ae8d3addec492
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2024-05-25 18:35:16 +0300

    adds header hojs style, fixes map description position

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,24 +1,7 @@
-// Menu button
-const menuBtn = document.getElementById('mobile-menu');
+const headerNav = document.querySelector('.header__nav');
+const menuBtn = document.querySelector('.header__mobile-menu');
 
-const onMenuClick = () => {
-  if (menuBtn.classList.contains('header__mobile-menu--opened')) {
-    menuBtn.classList.remove('header__mobile-menu--opened');
-  } else {
-    menuBtn.classList.add('header__mobile-menu--opened');
-  }
-};
+headerNav.classList.remove('header__nav--nojs');
 
-document.addEventListener('DOMContentLoaded', () => menuBtn.addEventListener('click', onMenuClick));
-
-
-// Slider
-/* const slider = document.getElementById('slider');
-const slideContainer = document.getElementById('img1');
-
-const onInputChange = () => {
-  slideContainer.style.width = `${slider.value}%`;
-  slider.style.setProperty('--pseudo-content', `${slider.value - 1}%`);
-};
-
-document.addEventListener('DOMContentLoaded', () => slider.addEventListener('change', onInputChange)); */
+const onMenuClick = () => menuBtn.classList.toggle('header__mobile-menu--opened');
+menuBtn.addEventListener('click', onMenuClick);
