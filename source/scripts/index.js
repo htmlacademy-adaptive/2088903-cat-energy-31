4545db67e02edaff45c965d28f8f96697f2e20c7
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2024-05-31 02:16:22 +0300

    final fixes

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,10 @@
 const headerNav = document.querySelector('.header__nav');
 const menuBtn = document.querySelector('.header__mobile-menu');
+const iframe = document.querySelector('iframe');
 
 headerNav.classList.remove('header__nav--nojs');
+
+iframe.style.display = 'block';
 
 const onMenuClick = () => menuBtn.classList.toggle('header__mobile-menu--opened');
 menuBtn.addEventListener('click', onMenuClick);
