83a2c8d069c0dc880222e6ac6449c1a10ee3ecd0
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2024-05-31 22:06:13 +0300

    fixes nojs

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,10 @@
 const headerNav = document.querySelector('.header__nav');
 const menuBtn = document.querySelector('.header__mobile-menu');
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('.contacts--nojs');
 
 headerNav.classList.remove('header__nav--nojs');
-
-iframe.style.display = 'block';
+menuBtn.classList.remove('header__mobile-menu--nojs');
+iframe.classList.remove('contacts--nojs');
 
 const onMenuClick = () => menuBtn.classList.toggle('header__mobile-menu--opened');
 menuBtn.addEventListener('click', onMenuClick);
